8bc753c3149b001c4e29bba55699017d908f40f2
Darncol <60666804+Darncol@users.noreply.github.com>
2022-08-23 16:07:19 +0300

    Metanit

---
+++ AspMetanit/AspMetanit/Notes.cs
@@ -0,0 +1,20 @@
+﻿namespace AspMetanitNotes
+{
+    public class Notes
+    {
+        /* минимальное API
+         * var builder = WebApplication.CreateBuilder(args); //специальный класс строитель
+         * var app = builder.Build(); //создание обьекта WebApplication
+         * app.MapGet("/", () => "Hello World!"); // с помощью WebApplication можно настроить всю инфраструктуру
+         * app.Run(); //необходим запуск приложения
+         */
+
+
+        /* await app.StartAsync();  
+         * await Task.Delay(10000); // ждать 10 секунд
+         * await app.StopAsync();
+         */
+
+
+    }
+}
